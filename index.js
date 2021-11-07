1939eb1da84949de361e36954cf56296748465b3
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-11-07 22:10:40 +0600

    Change .env

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
 // Firebase
 const admin = require("firebase-admin");
 const serviceAccount = require(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
 
 // Verify Token
 async function verifyToken(req, res, next) {
@@ -27,12 +34,6 @@ async function verifyToken(req, res, next) {
     };
     next();
 };
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const port = process.env.PORT || 5000;
 
 // MongoDB START
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwkey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
