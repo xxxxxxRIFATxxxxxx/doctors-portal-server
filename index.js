011d4282f26628dbb081769fe97ac91da2836bfb
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-11-07 12:15:21 +0600

    Add Firebase Admin

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
+// Firebase
+const admin = require("firebase-admin");
+const serviceAccount = require("./doctors-portal-client-private-key.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+// Verify Token
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        };
+
+    };
+    next();
+};
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -20,6 +44,24 @@ async function run() {
         await client.connect();
         const database = client.db("doctorsPortalDatabase");
         const usersCollection = database.collection("usersCollection");
+        const appointmentsCollection = database.collection("appointmentsCollection");
+
+        // GET APPOINTMENTS API
+        app.get('/appointments', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const date = req.query.date;
+            const query = { email: email, date: date }
+            const cursor = appointmentsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // POST APPOINTMENTS API
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.send(result)
+        });
 
         // POST USERS API
         app.post('/users', async (req, res) => {
@@ -40,6 +82,25 @@ async function run() {
             res.send(result);
         });
 
+        // SET A USER TO ADMIN API
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'Sorry you do not have access to make admin' });
+            };
+
+        });
+
         // CHECK USER ROLE
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
@@ -51,7 +112,6 @@ async function run() {
             }
             res.send(isAdmin);
         });
-
     }
 
     finally {
