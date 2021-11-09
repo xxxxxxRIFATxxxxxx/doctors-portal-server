e2f9dcf2f6b6ba98ded174bf202742d2b9f0e811
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-11-09 12:08:19 +0600

    Problem Solve

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ const port = process.env.PORT || 5000;
 
 // Firebase
 const admin = require("firebase-admin");
-// const serviceAccount = require(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
+const serviceAccount = require(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 
 // Verify Token
 async function verifyToken(req, res, next) {
-    // if (req.headers?.authorization?.startsWith('Bearer ')) {
-    //     const token = req.headers.authorization.split(' ')[1];
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
 
-    //     try {
-    //         const decodedUser = await admin.auth().verifyIdToken(token);
-    //         req.decodedEmail = decodedUser.email;
-    //     }
-    //     catch {
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
 
-    //     };
+        };
 
-    // };
-    // next();
+    };
+    next();
 };
 
 // MongoDB START
